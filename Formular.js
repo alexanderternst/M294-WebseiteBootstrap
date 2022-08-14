2da2b19613ef9cbf8dc9102448bffe804ab8aa24
alex0417a <96046094+alex0417a@users.noreply.github.com>
2022-08-14 09:53:56 +0200

    Formular JS V1.3
    Kommentare hinzugefügt.

--- Formular.js
+++ Formular.js
@@ -1,3 +1,4 @@
+// Definieren aller Variablen
 let date;
 let output;
 let prioritaet;
@@ -8,23 +9,31 @@ let email;
 let telefon;
 let dienstleistung;
 
+//Funktionn Datum aufrufen
 $(document).ready(function () {
     Datum();
 });
 
 
 function Datum(){
+    //Rufe function(e) auf wenn Formular Submitted wird
     $("#Formular").submit(function(e){
+        //Submite das Formular nicht (führe die action des Formulars nicht aus)
         e.preventDefault();
+        // methode date kreieren
         date = new Date();
+        // aktuelles datum speichern
         output = String(date.getDate()) + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
         //alert(output);
+        //Rufe Funktion Berechnung auf
         Berechnung();
     });
 }
 
 function Berechnung() {
+    // Speichere die prioritaet
     prioritaet = $("#Prioritaet").val();
+    // Rechne das Abholdatum aus
     switch (prioritaet) {
         case "tief":
             newDate = (date.getDate() + 12) + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
@@ -44,15 +53,18 @@ function Berechnung() {
         default:
             break;
     }
+    // Rufe Funktion Ausgabe auf
     Ausgabe();
 }
 
 function Ausgabe(){
+    // Speichere kundenname, email, telefon und dienstleistung
     kundenname = $("#kundename").val();
     email = $("#mail").val();
     telefon = $("#tel").val();
     dienstleistung = $("#Service").val();
 
+    // Gebe Werte des Formulars in vorher kreiertenm Heading und Paragraph aus (überprüfung fehlt noch)
     $("#AusgabeTitel").html('Auswahl');
     $("#AusgabeParagraph").html(
         ` <br> Kundename ${kundenname} <br>
@@ -67,6 +79,7 @@ function Ausgabe(){
 
 }
 
+// Test, Goal: wenn man cards drückt wählt es direkt den richtigen Service an
 function Landingpage1() {
     //$("#klein").click($("#Service").val("Kleiner-Service"));
     //$("#gross").click($("#Service").val("Grosser-Service"));
@@ -76,7 +89,3 @@ function Landingpage1() {
     //$("#heiss").click($("#Service").val("Heisswachsen"));
     
 }
-
-
-// Daten an Server schicken
-// Telefon und Email validieren
